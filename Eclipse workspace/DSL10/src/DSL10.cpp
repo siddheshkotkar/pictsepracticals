dded18ef54085b63917c5da8fb8bf8088b672cc9
siddheshkotkar <kotkar.sidh@gmail.com>
2022-01-08 00:14:06 +0530

    pr10 modified

--- Eclipse workspace/DSL10/src/DSL10.cpp
+++ Eclipse workspace/DSL10/src/DSL10.cpp
@@ -1,97 +1,179 @@
-#include<iostream>
+#include "iostream"
+#include "vector"
+
 using namespace std;
-#include<stack>
-class intopos
-{
+
+
+class Stack {
+	char *s;
+	int top = -1;
+
 public:
-	int prec(char c)
-	{
-		if(c=='^')
-		{
-			return 3;
 
-		}
-		else if(c=='/' || c=='*')
-		{
-			return 2;
-		}
-		else if(c=='+' || c=='-')
-		{
-			return 1;
-		}
-		else
-		{
-			return -1;
-		}
-	}
-	string infixtopostfix(string s)
-	{
-		stack<char> st;
-		string res;
-		for(int i=0;i<s.length();i++)
-		{
-			if((s[i]>='a' && s[i]<='z') || (s[i]>='A' && s[i]<='Z'))
-			{
-				res+=s[i];//directly add operand to result
-			}
-			else if(s[i]=='(')
-			{
-				st.push(s[i]);//directly push opening bracket in stack
-			}
-			else if(s[i]==')')
-			{
-				while(!st.empty() && st.top()!='(')
-				{
-					res+=st.top();//directly add elements other than ( in result
-					st.pop();//pop those same from stack
-				}
-				if(!st.empty())
-				{
-					st.pop();//pop the reamining ( bracket...
-				}
-			}
-			else
-			{
-				while(!st.empty() && prec(st.top())>=prec(s[i]))//check for operator precedance
-				{
-					res+=st.top();//if opeartor in stack is having greater precedance than current scanned operator then add the stack's operator to the result
-					st.pop();//pop the stack's operator (the previous one)(which is greater) from the stack
-				}
-			}
-
-		}
-		while(!st.empty())
-		{
-			res+=st.top();//check if there are any elements in stack, if present then add them to result
-			st.pop();//pop the same from stack
-		}
-		return res;//return the result...
-
+	Stack(int len) {
+		s = new char[len];
 	}
 
+	void push(char ele) {
+		s[++top] = ele;
+	}
+
+	string conversionHelper(char ele) {
+//		s[++top] = ele;
+//		cout<<ele<<endl;
+		string returnStr = "";
+		if(ele == '-' || ele == '+') {
+			if(top == -1) {
+				s[++top] = ele;
+//				cout<<returnStr<<"l23"<<endl;
+				return returnStr;
+			}
+			if(s[top] == '*' || s[top] == '/') {
+				returnStr += pop();
+//				cout<<returnStr<<"l28"<<endl;
+				return (returnStr + conversionHelper(ele));
+			}
+			else if(s[top] == '+' || s[top] == '-') {
+				returnStr += pop();
+//				cout<<returnStr<<"l33"<<endl;
+				return (returnStr + conversionHelper(ele));
+			}
+			else{
+				s[++top] = ele;
+//				cout<<returnStr<<"l37"<<endl;
+				return returnStr;
+			}
+		}
+		else{
+			if(top == -1) {
+				s[++top] = ele;
+//				cout<<returnStr<<"l44"<<endl;
+				return returnStr;
+			}
+			if(s[top] == '/' || s[top] == '*') {
+				returnStr += pop();
+//				cout<<returnStr<<"l49"<<endl;
+				return (returnStr + conversionHelper(ele));
+			}
+			s[++top] = ele;
+//			cout<<returnStr<<"l52"<<endl;
+			return returnStr;
+
+		}
+		return returnStr;
+	}
+
+	string popAllStack() {
+		string retStr = "";
+		while(top != -1) {
+			retStr += s[top--];
+		}
+		return retStr;
+	}
+
+	char pop() {
+		return s[top--];
+	}
 };
-int main()
-{
-	intopos in;
-	string expr;
-	int ch;
-	do{
-		cout<<"\nMENU\n"<<"1.Convert expression\n"<<"2.Evaluate the expression:\n"<<"\n3.Exit\n"<<"Enter your choice:\n";
-		cin>>ch;
-		switch(ch)
-		{
-		case 1:
-			cout<<"\nEnter expression:\n";
-			cin>>expr;
-			string result=in.infixtopostfix(expr);
-			cout<<"\nPostfix expression is: "<<result;
-			cout<<endl;
-			break;
-		case 2:
-//			in.evaluate(expr);
-			break;
-		case 3:
-			break;
+
+class ExpressionConv {
+	string expression, postfix = "";
+
+public:
+	void printMenu() {
+		cout<<"\nMENU";
+		cout<<"\n1. Print Postfix Expression";
+		cout<<"\n2. Evaluate given expression";
+		cout<<"\n3. EXIT";
+	}
+
+	void convertExpression() {
+		Stack s(expression.length());
+//		cout<<expression;
+		for(int i = 0; i < expression.length(); i++) {
+			char current = expression[i];
+			if(current == '+' || current == '-' || current == '*' || current == '/') {
+				string str = s.conversionHelper(current);
+				postfix += str;
+//				cout<<"postfix: "<<postfix<<"and"<<str<<endl;
+			}
+			else{
+				postfix += current;
+			}
 		}
-	}while(ch!=3);
+		postfix += s.popAllStack();
+		cout<<postfix<<endl;
+	}
+
+	int exec(char operation, int a, int b) {
+		switch(operation){
+		case '+':
+			return a+b;
+		case '-':
+			return b-a;
+		case '*':
+			return a*b;
+		case '/':
+			return b/a;
+		default:
+			return 0;
+		}
+	}
+	void evaluatePostfix() {
+		Stack s(postfix.length());
+		vector<int> evals;
+		for(int i = 0; i < postfix.length(); i++) {
+			char current = postfix[i];
+			if(current == '+' || current =='-' || current == '/' || current == '*') {
+//				int a = int(s.pop())-48;
+//				int b = int(s.pop());
+				int a = evals.back();
+				evals.pop_back();
+				int b = evals.back();
+				evals.pop_back();
+
+				cout<<a<<current<<b<<endl;
+
+				int ans = exec(current, a, b);
+
+//				s.push((ans+"")[0]);
+				evals.push_back(int(ans));
+			}else{
+//				s.push(postfix[i]);
+				evals.push_back(int(postfix[i])-48);
+			}
+		}
+//		int ans = evals.pop_back();
+		cout<<"\nExpression evaluated to: "<<evals.back();
+	}
+	void execute() {
+		cout<<"\nEnter Expression: ";
+		cin>>expression;
+		bool isExit = false;
+		while(!isExit) {
+			printMenu();
+			cout<<"\nEnter Choice: ";
+			int ch;
+			cin>>ch;
+			switch(ch) {
+			case 1:
+				convertExpression();
+				break;
+			case 2:
+				evaluatePostfix();
+				break;
+			case 3:
+				isExit = true;
+				break;
+			}
+		}
+	}
+};
+
+
+int main() {
+	ExpressionConv c;
+	c.execute();
+
+	return 0;
 }
