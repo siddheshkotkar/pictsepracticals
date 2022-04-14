7d76f8409233e91905d40937bffe2ad6f491777a
siddheshkotkar <kotkar.sidh@gmail.com>
2022-04-14 13:37:53 +0530

    practical 4 completed

--- ADS/practical4.cpp
+++ ADS/practical4.cpp
@@ -10,7 +10,7 @@ class get_details
 	public:
 		long long int x,temp;
 		details d[MAX];
-		
+	
 		void initialise()
 		{
 			for(int i=0;i<MAX;i++)
@@ -19,44 +19,141 @@ class get_details
 			}
 		}
 		
-		void accept()
+		int search(details a[MAX],long long int fkey)
 		{
-			int index;
-			char ch;
-			do{
-			
-			cout<<"\n Enter phone number:";
-			cin>>x;
-			index=x%MAX;
-//			cout<<index;
 			for(int i=0;i<MAX;i++)
 			{
+				if(a[i].phNo==fkey)
+				{
+					return i;
+				}
+			}
+			return 0;
+		}
+		
+		int find_duplicate(details a[MAX],long long int fkey)
+		{
+			for(int i=0;i<MAX;i++)
+			{
+				if(d[i].phNo==x)
+					return 1;
+					
+			}
+			return 0;
+		}
+		
+		void hash_Nreplacement()
+		{
+			char ch;
+			do{
+			cout<<"\nEnter phone number:";
+			cin>>x;
+			int dup=find_duplicate(d,x);
+			if(dup==1)
+			{
+			cout<<"\nDuplicate entered!";
+			continue;
+			}
+			else
+			{
+			
+				int index;
+				index=(x%MAX);
 				if(d[index].phNo==0)
 				{
-					d[index].phNo=x;
-					break;
-				}
-				else if(d[index].phNo!=0)
-				{
-					temp=d[index].phNo;
-					d[index].phNo=x;
-					for(int i=index;i<MAX;i++)
-					{
-						if(d[i].phNo==0)
-						{
-							d[i].phNo=temp;
-						}
-					}
+				d[index].phNo=x;
+				
 				}
 				else
 				{
-					index=index+1;
+				for(int i=index;i<MAX;i++)
+				{
+					if(d[i].phNo==0)
+					{
+						d[i].phNo=x;
+						break;
+					}
+				}
+				
+			}
+		}
+			cout<<"\n Do you want to continue adding?(y/n):";
+			cin>>ch;
+			}while(ch!='n');
+			cout<<"\n Enter element to search:";
+			cin>>temp;
+			int ans=search(d,temp);
+			if(ans!=0)
+				cout<<"\n Phone number found at location "<<ans;
+			else
+				cout<<"\n Element not found";
+
+		}
+		
+		void hash_Wreplacement()
+		{
+			char ch;
+			do{
+			
+			cout<<"\nEnter phone number:";
+			cin>>x;
+			int dup=find_duplicate(d,x);
+			if(dup==1)
+			{
+			cout<<"\nDuplicate entered!";
+			continue;
+			}
+			else
+			{
+			
+			int index,tmp_index;
+			index=(x%MAX);
+			
+			if(d[index].phNo==0)
+			{
+				d[index].phNo=x;
+				tmp_index=index;
+			}
+			else if(d[index].phNo!=0 && index==tmp_index)
+			{
+				for(int i=index;i<MAX;i++)
+				{
+					if(d[i].phNo==0)
+					{
+						d[i].phNo=x;
+						break;
+					}
 				}
 			}
-			cout<<"(y/n):";
-			cin>>ch;
-		}while(ch!='n');
+			else
+			{
+				if(d[index].phNo!=0)
+				{
+					temp=d[index].phNo;
+					d[index].phNo=x;
+				}
+				for(int i=index;i<MAX;i++)
+				{
+					if(d[i].phNo==0)
+					{
+						d[i].phNo=temp;
+						break;
+					}
+				}
+				
+			}
 		}
+		cout<<"\n Do you want to continue(y/n):";
+		cin>>ch;
+		}while(ch!='n');
+		cout<<"\n Enter element to search:";
+		cin>>temp;
+		int ans=search(d,temp);
+		if(ans!=0)
+			cout<<"\n Phone number found at location "<<ans;
+		else
+			cout<<"\n Element not found";
+	}
 		void print()
 		{
 			for(int i=0;i<MAX;i++)
@@ -70,23 +167,27 @@ int main()
 {
 	get_details g;
 	g.initialise();
-	char choice;
+	int choice;
 	do
 	{
-		cout<<"\n1.Accept";
-		cout<<"\n2.Print";
-		cout<<"\n3.Exit";
+		cout<<"\n1.Without replacement";
+		cout<<"\n2.With replacement";
+		cout<<"\n3.Print";
+		cout<<"\n4.Exit";
 		cout<<"\nEnter your choice:";
 		cin>>choice;
 		switch(choice)
 		{
 			case 1:
-				g.accept();
+				g.hash_Nreplacement();
 				break;
 			case 2:
+				g.hash_Wreplacement();
+				break;
+			case 3:
 				g.print();
 				break;
 			
 		}
-	}while(choice!=3);
+	}while(choice!=4);
 }
